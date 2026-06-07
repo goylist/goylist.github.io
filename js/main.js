9a057877715c23478d7eb486d5dff3b2cb9da784
goylist <tashaka.v.ustata@gmail.com>
2026-06-07 12:51:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import routes from './routes.js';
 
 export const store = Vue.reactive({
     dark: JSON.parse(localStorage.getItem('dark')) || false,
+    activeModal: '', // <-- This is the magic tracker that allows the popups to open!
     toggleDark() {
         this.dark = !this.dark;
         localStorage.setItem('dark', JSON.stringify(this.dark));
